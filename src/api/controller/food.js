20bbad5892663e30b3561f207f2f638d55229ca9
Diane1996 <heyanxia1996@outlook.com>
2018-05-07 23:29:02 +0800

    '登录'

--- src/api/controller/food.js
+++ src/api/controller/food.js
@@ -12,4 +12,21 @@ module.exports = class extends Base {
       this.success({result: data});
     }
   }
+
+  async addAction() {
+    var data = this.get();
+    var name = data.name;
+    var openid = data.openid;
+    const data = await this.model('food')
+      .where({openid})
+      .select();
+
+    if (think.isEmpty(data)) {
+      this.ctx.response.body = '数据不存在请重试';
+      this.fail('402051', 'Food_empty_Error');
+    } else {
+      this.ctx.response.body = data;
+      this.success({result: data});
+    }
+  }
 }
